ddd1ca6012028cb1d558fbb8a34dd4de641f0c88
Muhammad Nur Ichsan <69494417+muhnurichsan@users.noreply.github.com>
2020-12-09 15:38:16 +0700

    make documentation

--- server.js
+++ server.js
@@ -5,15 +5,17 @@ let users = []
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-//all users
+//get all users
 app.get("/user", (req, res) => {
     res.status(200).json({
         users
     })
 })
 
+//create user data
 app.post("/user", async(req, res) => {
     try {
+        //hashed password asynchronously
         const hashedPassword = await bcrypt.hash(req.body.password, 10)
         const user = {
             username: req.body.username,
@@ -25,12 +27,14 @@ app.post("/user", async(req, res) => {
         res.sendStatus(500)
     }
 })
+//login with user data
 app.post("/user/login", async(req, res) => {
     const user = users.find(user => user.username === req.body.username)
     if (user == null) {
         return res.status(404).send("can't find any users related")
     }
     try {
+        //compare password asynchronously
         if (await bcrypt.compare(req.body.password, user.password)) {
             return res.send("Success Login")
         } else {
@@ -40,4 +44,6 @@ app.post("/user/login", async(req, res) => {
         return res.statusCode(404)
     }
 })
+
+//server running or localhost:2001
 app.listen(2001)
